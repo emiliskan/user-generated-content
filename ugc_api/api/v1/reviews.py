6928e2a7dcc1aa3a510e9e4af5ea4f37cd22ce0d
Эмиль Искандеров <emilagamamedov@gmail.com>
2021-09-04 23:46:17 +0300

    added review api

--- ugc_api/api/v1/reviews.py
+++ ugc_api/api/v1/reviews.py
@@ -1,29 +1,117 @@
 import logging
+import uuid
+from http import HTTPStatus
+from typing import List
 
-from fastapi import APIRouter
-from pymongo import MongoClient
+from fastapi import APIRouter, Query, Depends, HTTPException, Response
+from fastapi.security import HTTPBearer, HTTPBasicCredentials
 
-from models.review import Review
+from models.base import PydanticObjectId
+from models import Review, ReviewAnswer, ReviewQuery
+from services.exceptions import DocumentNotFound
+from services.reviews import UserReviewsService, get_user_reviews_service
+from services.auth import get_user_id, AuthServiceUnavailable
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
-
-@router.post("/reviews")
-async def create_review():
-    pass
+auth_scheme = HTTPBearer()
 
 
-@router.get("/reviews/{review_id}")
-async def get_review():
-    pass
+@router.get(
+    "/reviews",
+    description="Reviews list.",
+    response_model=List[ReviewAnswer]
+)
+async def get_reviews(
+        query: ReviewQuery,
+        service: UserReviewsService = Depends(get_user_reviews_service),
+):
+    return await service.search(query)
 
 
-@router.delete("/reviews/{review_id}")
-async def delete_review():
-    pass
+@router.post(
+    "/reviews",
+    description="Create review.",
+    response_model=ReviewAnswer
+)
+async def create_review(
+        review: Review,
+        service: UserReviewsService = Depends(get_user_reviews_service),
+        credentials: HTTPBasicCredentials = Depends(auth_scheme),
+):
+    token = credentials.credentials
+    try:
+        user_id = await get_user_id(token)
+    except AuthServiceUnavailable:
+        raise HTTPException(status_code=HTTPStatus.SERVICE_UNAVAILABLE)
+
+    return await service.add(user_id, review)
 
 
-@router.patch("/reviews/{review_id}")
-async def update_review():
-    pass
+@router.get(
+    "/reviews/{review_id}",
+    description="Get review.",
+    response_model=ReviewAnswer
+)
+async def get_review(
+        review_id: PydanticObjectId = Query(None, description="Review ID"),
+        service: UserReviewsService = Depends(get_user_reviews_service),
+):
+
+    try:
+        review = await service.get(review_id)
+    except DocumentNotFound:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
+
+    return review
+
+
+@router.delete(
+    "/reviews/{review_id}",
+    description="Remove review."
+)
+async def delete_review(
+        review_id: str = Query(None, description="Review ID"),
+        service: UserReviewsService = Depends(get_user_reviews_service),
+        credentials: HTTPBasicCredentials = Depends(auth_scheme),
+):
+    token = credentials.credentials
+    try:
+        user_id = await get_user_id(token)
+    except AuthServiceUnavailable:
+        raise HTTPException(status_code=HTTPStatus.SERVICE_UNAVAILABLE)
+
+    review_id = PydanticObjectId(review_id)
+    try:
+        await service.remove(user_id, review_id)
+    except DocumentNotFound:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
+
+    return Response(status_code=200)
+
+
+@router.patch(
+    "/reviews/{review_id}",
+    description="Update review.",
+    response_model=Review
+)
+async def update_review(
+        review: Review,
+        review_id: str = Query(None, description="Review ID"),
+        service: UserReviewsService = Depends(get_user_reviews_service),
+        credentials: HTTPBasicCredentials = Depends(auth_scheme),
+):
+    token = credentials.credentials
+    try:
+        user_id = await get_user_id(token)
+    except AuthServiceUnavailable:
+        raise HTTPException(status_code=HTTPStatus.SERVICE_UNAVAILABLE)
+
+    review_id = PydanticObjectId(review_id)
+    try:
+        updated = await service.update(user_id, review_id, review)
+    except DocumentNotFound:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
+
+    return updated
