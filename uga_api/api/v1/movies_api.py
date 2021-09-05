c007618fe68597cef1fa2a44bf851d257d3e508c
Natalia <maltseva@arrival.com>
2021-09-05 21:28:57 +0300

    temporary removed connecting to auth

--- uga_api/api/v1/movies_api.py
+++ uga_api/api/v1/movies_api.py
@@ -23,13 +23,14 @@ async def save_movie_progress(
 
     if Authorization is None:
         raise HTTPException(status_code=HTTPStatus.NETWORK_AUTHENTICATION_REQUIRED, detail="Auth token required.")
-
-    try:
+    # temporary remove connecting to Auth
+    '''try:
         user_info = await get_user_info(Authorization)
     except AuthServiceUnavailable:
         raise HTTPException(status_code=HTTPStatus.SERVICE_UNAVAILABLE)
     except UserNotFound:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
 
-    await movie_service.save_movie_progress(movie_id, user_info.id, viewed_frame)
+    await movie_service.save_movie_progress(movie_id, user_info.id, viewed_frame)'''
+    await movie_service.save_movie_progress(movie_id, "87453e9b-e9d6-4caa-bbe2-9ffda193a999", viewed_frame)
 
