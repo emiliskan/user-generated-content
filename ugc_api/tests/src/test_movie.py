cd4d86513486d12988f11dc591f98f9c44e37b4c
Эмиль Искандеров <emilagamamedov@gmail.com>
2021-09-06 23:46:05 +0300

    review tests

--- ugc_api/tests/src/test_movie.py
+++ ugc_api/tests/src/test_movie.py
@@ -5,9 +5,13 @@ API_URL = f"{API_SERVICE_URL}/api/{API}/scores/movies"
 
 
 @pytest.mark.asyncio
-async def test_movie_score(make_get_request):
+async def test_movie_score(auth, make_get_request):
+
+    headers = {
+        "Authorization": f"Bearer {auth}"
+    }
 
     movie_id = "3fa85f64-5717-4562-b3fc-2c963f66afa6"
-    response = await make_get_request(f"{API_URL}/{movie_id}")
+    response = await make_get_request(f"{API_URL}/{movie_id}", headers=headers)
 
     assert response.status == 200, "Couldn't find movie."
